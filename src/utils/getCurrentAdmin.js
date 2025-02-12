aa3ec3bd50a540b123855be4321fc040f1688c2c
armandmorin <armandmorin@gmail.com>
2025-02-11 20:02:09 -0500

    Initial commit

--- src/utils/getCurrentAdmin.js
+++ src/utils/getCurrentAdmin.js
@@ -1,9 +1,7 @@
-export async function getCurrentAdmin() {
+export function getCurrentAdmin() {
   const storedUser = localStorage.getItem('user');
   if (storedUser) {
-    // Return parsed user data from localStorage
     return JSON.parse(storedUser);
   }
-  // If not found, return null to trigger login prompt
   return null;
 }
